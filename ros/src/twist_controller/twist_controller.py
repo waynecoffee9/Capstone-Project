fff536ac7ff5066c12df064c47579e00dafc5693
Wayne Chen <waynechen1210@gmail.com>
2019-12-05 16:14:40 -0700

    General Maintenance
    Added comments

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -4,7 +4,7 @@ from lowpass import LowPassFilter
 from yaw_controller import YawController
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
-MAX_BRAKE = 700
+MAX_BRAKE = 700 # brake torque required to maintain car stopped
 
 class Controller(object):
 	def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, 
@@ -12,20 +12,25 @@ class Controller(object):
 				steer_ratio, max_lat_accel, max_steer_angle):
 		# TODO: Implement
 		self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
-		
+		## PID parameters for throttle
 		Kp = 0.3
 		Ki = 0.1
 		Kd = 0.0
 		mn = 0.0 #min throttle
 		mx = 0.23 #max throttle
+		
+		## PID parameters for braking
 		Kp_b = 60.0
 		Ki_b = 0.0
 		Kd_b = 10.0
 		mn_b = 0.0 #min brake
+		
+		# setup two PIDs
 		self.throttle_controller = PID(Kp, Ki, Kd, mn, mx)
 		self.brake_controller = PID(Kp_b, Ki_b, Kd_b,mn_b,MAX_BRAKE)
+		
 		tau = 0.5 #1/(2pi*tau) = cutoff freq
-		ts = 0.02 # sampling time
+		ts = 0.02 # sampling time, this should reflect refresh rate from dbw_node
 		self.vel_lpf = LowPassFilter(tau, ts)
 		
 		self.vehicle_mass = vehicle_mass
@@ -58,15 +63,14 @@ class Controller(object):
 		
 		throttle = self.throttle_controller.step(vel_error, sample_time)
 		brake = 0
-		
+		# when car is supposed to stop and current velocity is small, throw on max brake
 		if linear_vel == 0.0 and current_vel < 0.2:
 			throttle = 0
-			brake = 700
-		
+			brake = MAX_BRAKE
+		# when car is going faster than target speed, let go off throttle and apply brake
 		elif throttle < 0.1 and vel_error < 0:
 			throttle = 0
 			brake = self.brake_controller.step(-vel_error, sample_time)
-			
-		#return 1., 0., 0.
+		
 		return throttle, brake, steering
 
